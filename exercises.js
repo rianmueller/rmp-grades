417167b42f1cddcd4f66f07323be102353672698
madycakes <viclee02@gmail.com>
2019-03-20 11:47:35 -1000

    adds exercises

---
+++ exercises.js
@@ -0,0 +1,18 @@
+//Create a Revealing Module Pattern that will have the following data and methods:
+
+//data
+//scores 65, 76, 100, 88, 17, 55
+
+//methods
+// 1. finds the sum of all scores
+// 2. finds the avg of all scores
+// 3. finds the highest score
+// 4. finds the lowest score
+// 5. generates a random score
+// 6. finds the number of passing scores above 70
+// 7. finds the first score in the list 
+// 8. finds the last score in the list 
+// 9. generates a new score list with a 5% increase for each score 
+// 10.generates a list of scores that are odd numbered
+
+
