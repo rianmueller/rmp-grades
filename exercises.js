177e1e7f14e2d0e15a8345aab0d865caced12bd7
Rian Mueller <rian@Rians-MacBook-Pro-2.local>
2019-06-21 16:08:40 -1000

    1

--- exercises.js
+++ exercises.js
@@ -15,4 +15,66 @@
 // 9. generates a new score list with a 5% increase for each score 
 // 10.generates a list of scores that are odd numbered
 
+var fooModule = (function(){
+    var scores = [65,76,100,88,17,55];
 
+    function sum(){
+        var total = 0;
+        for (i = 0; i < scores.length; i++) {
+            total = total + scores[i];
+        }
+        return total;
+    }
+
+    return {
+        sum: sum
+    };
+})();
+
+    // function avg(){
+    //     let total = 0;
+    //     let avg;
+    //     for (i = 0; i < scores.length; i++) {
+    //         total = total + scores[i];
+    //         avg = total / (scores.length + 1);
+    //     }
+    //     return avg;
+    // }
+
+    // function highest(){
+    //     return Math.max(scores);
+    // }
+
+    // function lowest(){
+    //     return Math.min(scores);
+    // }
+
+    // function random(){
+    //     return Math.floor(Math.random() * scores.length);
+    // }
+
+
+
+// function calculatorModule(){
+//     let calculator = {};
+//     let memory = 0;
+//     let total = 0;
+
+//     calculator.validation = function(number){
+//         if (typeof number !== 'number'){
+//             throw 'Error';
+//         }
+//     };
+
+//     calculator.load = function(number){
+//         this.validation(number);
+//         total = number;
+//         return total;
+//     };
+
+//     calculator.getTotal = function(){
+//         return total;
+//     };
+
+//     return calculator;
+// };
